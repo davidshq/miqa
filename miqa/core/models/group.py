188ef3c0f55abaa7b61b743ff37d44288a8662e3
Dave Mackey <dave@davemackey.net>
2022-08-11 11:01:22 -0400

    Add friendly name for Group model

--- miqa/core/models/group.py
+++ miqa/core/models/group.py
@@ -7,3 +7,6 @@ class Group(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=255, blank=False)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
