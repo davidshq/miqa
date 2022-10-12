f4d7e17341494cebf5e848407a3401ffa9348cbb
Dave Mackey <dave@davemackey.net>
2022-10-12 11:10:10 -0400

    Merge origin/master changes to django.ts/router.ts

--- web_client/src/django.ts
+++ web_client/src/django.ts
@@ -184,6 +184,11 @@ const djangoClient = {
     const response = await apiClient.delete(`/experiments/${experimentId}/lock`);
     return response?.data;
   },
+  async deleteExperiment(experimentId: string): Promise<ResponseData> {
+    if (!experimentId) return undefined;
+    const response = await apiClient.delete(`/experiments/${experimentId}`);
+    return response?.data;
+  },
   async scans(experimentId: string): Promise<Scan[]> {
     if (!experimentId) return undefined;
     const response = await apiClient.get('/scans', {
