3eee2cbacdd0efdcaf8fb7859b3a131c382daf0f
Dave Mackey <dave@davemackey.net>
2023-02-10 16:38:08 -0500

    Add Vuetify plugin file, remove vuetify shim

--- web_client/src/shims-vue.d.ts
+++ web_client/src/shims-vue.d.ts
@@ -1,15 +1,5 @@
 declare module 'vue' {
   import Vue from 'vue';
-  import { PropType } from 'vue';
-  function ref<T>(value: T): Ref<T>;
 
   export default Vue;
-  export { PropType, ref };
-}
-
-declare module '*.vue' {
-  import { Component } from 'vue';
-  var component: Component;
-
-  export default component;
 }
