e7252b067a09746a73165daf913cf1c8bd8edd53
Dave Mackey <dave@davemackey.net>
2023-02-12 17:05:42 -0500

    Cleaning of package.json including removal of Vuetify (due to circular dependencies with Vue)
    Update Vue config for 3.x

--- web_client/vue.config.js
+++ web_client/vue.config.js
@@ -77,5 +77,19 @@ module.exports = {
   },
   chainWebpack: (config) => {
     vtkChainWebpack(config);
+    config.resolve.alias.set('vue', '@vue/compat');
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
   },
 };
