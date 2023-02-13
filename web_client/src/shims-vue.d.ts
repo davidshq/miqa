c7766bbef54dc1e1d8a15dfc21234e27f4375ee3
Dave Mackey <dave@davemackey.net>
2023-02-13 15:08:22 -0500

    Update `shims-vue.d.ts` and `vue.config.js` with Vue 3 compatibility changes

--- web_client/src/shims-vue.d.ts
+++ web_client/src/shims-vue.d.ts
@@ -1,15 +1,8 @@
 declare module 'vue' {
-  import Vue from 'vue';
-  import { PropType } from 'vue';
-  function ref<T>(value: T): Ref<T>;
-
-  export default Vue;
-  export { PropType, ref };
-}
-
-declare module '*.vue' {
-  import { Component } from 'vue';
-  var component: Component;
-
-  export default component;
+  import { CompatVue } from '@vue/runtime-dom'
+  const Vue: CompatVue
+  export default Vue
+  export * from '@vue/runtime-dom'
+  const { configureCompat } = Vue
+  export { configureCompat }
 }
--- web_client/vue.config.js
+++ web_client/vue.config.js
@@ -76,6 +76,20 @@ module.exports = {
     };
   },
   chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat');
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => ({
+        ...options,
+        compilerOptions: {
+          compatConfig: {
+            MODE: 2,
+          },
+        },
+      }));
+
     vtkChainWebpack(config);
   },
 };
