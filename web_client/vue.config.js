866107beec544ef55ee5b925d3a327dce4d6a543
Dave Mackey <dave@davemackey.net>
2023-02-21 15:30:42 -0500

    Fix issue with `devServer.host`

--- web_client/vue.config.js
+++ web_client/vue.config.js
@@ -12,8 +12,7 @@ console.log(process.env.PUBLIC_ADDRESS);
 module.exports = {
   devServer: {
     port: 8081,
-    // TODO: Why isn't process.env.PUBLIC_ADDRESS working?
-    host: '127.0.0.1',
+    host: process.env.PUBLIC_ADDRESS,
   },
   lintOnSave: false,
   publicPath: process.env.VUE_APP_STATIC_PATH,
