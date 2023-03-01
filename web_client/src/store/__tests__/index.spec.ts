76084d7d52bb342d572694ac02e1c61307036382
Dave Mackey <dave@davemackey.net>
2023-03-01 11:48:48 -0500

    Add failing test for proxyNum support on SET_CURRENT_VTK_INDEX_SLICES

--- web_client/src/store/__tests__/index.spec.ts
+++ web_client/src/store/__tests__/index.spec.ts
@@ -4,7 +4,7 @@ import cloneDeep from 'lodash/cloneDeep';
 import { storeConfig } from "../../store";
 
 describe('Vuex Mutations', () => {
-  test('"SET_CURRENT_VTK_INDEX_SLICES should set the current vtk index slices"', () => {
+  test('SET_CURRENT_VTK_INDEX_SLICES should set the current vtk index slices', () => {
     const localVue = createLocalVue();
     localVue.use(Vuex);
     const theStore = cloneDeep(storeConfig);
@@ -19,4 +19,14 @@ describe('Vuex Mutations', () => {
     store.commit('SET_CURRENT_VTK_INDEX_SLICES', { indexAxis: 'k', value: 1});
     expect(store.state.kIndexSlice).toBe(1);
   });
+
+  test('SET_CURRENT_VTK_INDEX_SLICES should support multiple proxies', () => {
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
+    const theStore = cloneDeep(storeConfig);
+    const store = new Vuex.Store(theStore);
+    expect(store.state.iIndexSlice[0]).toBe(0);
+    store.commit('SET_CURRENT_VTK_INDEX_SLICES', { indexAxis: 'i', value: 1, proxyNum: 0});
+    expect(store.state.iIndexSlice[0]).toBe(1);
+  })
 });
