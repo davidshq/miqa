cfe117bf2ead408413a4f4b84e958d4e914b9278
Dave Mackey <dave@davemackey.net>
2023-03-08 15:19:34 -0500

    Add basic solo scan view to troubleshoot crosshairs

--- web_client/src/router.ts
+++ web_client/src/router.ts
@@ -4,6 +4,7 @@ import Router from 'vue-router';
 import Projects from './views/Projects.vue';
 import Scan from './views/Scan.vue';
 import CompareScans from "./views/CompareScans.vue";
+import SoloScan from './views/SoloScan.vue';
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/comparescans',
       name: 'comparescans',
       component: CompareScans,
+    },
+    {
+      path: '/soloscan',
+      name: 'soloscan',
+      component: SoloScan,
     },
     // Order matters
     {
