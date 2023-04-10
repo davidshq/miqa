05f052f7d08b304dd6751122527fae3e59f20784
Dave Mackey <dave@davemackey.net>
2023-04-10 13:27:01 -0400

    Commenting out vuetify/recommended for the moment

--- web_client/tests/basic-test.spec.ts
+++ web_client/tests/basic-test.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:8081/');
-  await page.goto('http://localhost:8081/#/');
   await page.goto('http://localhost:8000/accounts/login/?next=/oauth/authorize/%3Fredirect_uri%3Dhttp%253A%252F%252Flocalhost%253A8081%252F%26client_id%3DcBmD6D6F2YAmMWHNQZFPUr4OpaXVpW5w4Thod6Kj%26response_type%3Dcode%26state%3Dy5Nq6SATrH%26response_mode%3Dquery%26code_challenge%3DSwZrfGNbQD1jyielzKNvX2d3c1A8CtQuVCjsYE4BtCA%26code_challenge_method%3DS256');
   await page.getByPlaceholder('E-mail address').click();
   await page.getByPlaceholder('E-mail address').fill('user@localhost.local');
